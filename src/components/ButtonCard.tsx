64a54814863da18d2770a6215761c94d7664cb5f
redsylx <fakhrialbari.red@gmail.com>
2024-08-06 16:52:12 +0700

    add hook to call api

---
+++ src/components/ButtonCard.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import useCardAuth from "../hooks/HookCard"
+
+export default () => {
+    const [authResult, setAuthResult] = useState("");
+    const cardAuth = useCardAuth();
+
+    const click = async () => {
+        const result = await cardAuth();
+        const msg = result ?? "ERR";
+        setAuthResult(msg);
+    }
+
+    return (
+        <button className="custom-button" onClick={click}>Auth : {authResult}</button>
+    )
+}
